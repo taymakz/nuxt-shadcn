61ac1c697d4e815f2fb1e0fab228bf3b288d7b2a
TaymaZ <taymazak1382@gmail.com>
2024-01-18 21:19:45 +0330

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="min-h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div class="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <p class="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Nuxt Starter
+      </p>
+      <div class="absolute top-2 right-2">
+        <BaseToggleTheme />
+      </div>
+    </div>
+  </div>
+</template>
