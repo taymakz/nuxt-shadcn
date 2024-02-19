d251e326fb203c0ddf5f79de6cd98eabe446c2d7
TaymaZ <taymazak1382@gmail.com>
2024-02-19 22:00:38 +0330

    feat: changed Colors to variable colors

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div>
-    <div class="min-h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div class="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      <p class="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+    <div class="min-h-screen w-full bg-background  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div class="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <p class="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-emerald-200 to-text py-8">
         Nuxt - Shadcn Starter
       </p>
       <div class="absolute top-2 left-2">
