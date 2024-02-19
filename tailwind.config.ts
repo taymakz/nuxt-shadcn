e9041da1024d70cf5a98132b923981f1970eb55c
TaymaZ <71381670+taymakz@users.noreply.github.com>
2024-02-19 20:53:31 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,43 +35,23 @@ export default {
         iranyekan: 'IRANYekan',
       },
 
-      colors: {
-        white: '#FEFEFF',
-        black: '#0D0D0D',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+       colors: {
+      white: '#FEFEFF',
+      black: '#0D0D0D',
+      border: 'hsl(var(--border))',
+      ring: 'hsl(var(--ring))',
+      background: 'hsl(var(--background))',
+      warning: 'hsl(var(--warning))',
+      success: {
+        DEFAULT: 'hsl(var(--success))',
+        secondary: 'hsl(var(--success-secondary))',
       },
+      text: 'hsl(var(--text))',
+      primary: 'hsl(var(--primary))',
+      secondary: 'hsl(var(--secondary))',
+      destructive: 'hsl(var(--destructive))',
+      muted: 'hsl(var(--muted))',
+  },
 
       borderRadius: {
         lg: 'var(--radius)',
