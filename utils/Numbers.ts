c50c7139c4324a2a9d3350f417a2f0dfe22e3ef9
TaymaZ <taymazak1382@gmail.com>
2024-02-20 22:04:42 +0330

    feat: add utils composable

--- utils/Numbers.ts
+++
@@ -1,6 +0,0 @@
-export function splitNumber(value: number | string | null) {
-  if (value === null || value === undefined || value === 0)
-    return '0'
-
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
