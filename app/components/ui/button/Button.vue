df9104a65019e2f1e1a1ca2ce111d8e52b6b0bc8
TaymaZ <taymazak1382@gmail.com>
2024-10-10 18:53:04 +0330

    feat: button completed

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
+import { cn } from '@/utils/cn'
 import { Primitive, type PrimitiveProps } from 'radix-vue'
 import { type ButtonVariants, buttonVariants } from '.'
-import { cn } from '@/utils/cn'
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  loading?: boolean
+  disabled?: boolean
+  hideContentOnLoading?:boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -20,7 +23,9 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="props.disabled || loading"
   >
-    <slot />
+    <slot v-if="!loading || (loading && !hideContentOnLoading)"/>
+    <Icon v-if="loading" name="lucide-loader-2" class="animate-spin" :size="18" />
   </Primitive>
 </template>
